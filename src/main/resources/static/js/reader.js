74000cb9677dc38831676819627b50ad619a4fa8
lyh-ADT <1043970238@qq.com>
2020-09-08 08:49:27 +0800

    添加 reader.html 章节显示和封面显示，book.html 跳转

--- src/main/resources/static/js/reader.js
+++ src/main/resources/static/js/reader.js
@@ -11,6 +11,9 @@ function loadNextChapter(){
     content.appendChild(volume);
 
     volume.className = "volume";
+    // 第一个是封面
+    const index = document.querySelectorAll(".volume").length - 1;
+    volume.innerHTML = `<h2>第${index}章</h2>`;
 
     for(let i=0; i < 10; i++) {
         const p = document.createElement("p");
@@ -20,6 +23,25 @@ function loadNextChapter(){
     }
 
 }
+
+const bookId = /id=(\d+)/.exec(location.search)[1];
+
+const bookInfoVue = new Vue({
+    el: ".index",
+    data: {
+        book: {}
+    },
+    mounted: () => {
+        $.ajax({
+            url: `/book/${bookId}`,
+            method: "GET",
+            type: "json",
+            success: (data) => {
+                bookInfoVue.book = data;
+            }
+        })
+    }
+})
 
 window.onscroll = function(e) {
     if(isAtBottom()){
