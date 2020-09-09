7cc2b318935bc4cc8c27aa799b2382aea341d591
lyh-ADT <1043970238@qq.com>
2020-09-09 16:17:15 +0800

    添加 数据库 bookshell表

--- etc/create-table.sql
+++ etc/create-table.sql
@@ -20,3 +20,11 @@ create table account(
     pwd varchar(100) not null,
     email varchar(200) not null unique
 );
+
+create table bookshell(
+    bsid int primary key auto_increment,
+    aid int not null,
+    bid int not null,
+    constraint FK_bookshell_aid foreign key(aid) references account(aid),
+    constraint FK_bookshell_bid foreign key(bid) references book(bid)
+);
